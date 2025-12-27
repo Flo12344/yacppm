3f33ba05d49e6577122d9aeb72aa9a91ce293f42
Flouttre <florent.daerden38@gmail.com>
2025-12-27 20:02:52 +0100

    Fixed run command

--- src/cli/commands/run.hpp
+++ src/cli/commands/run.hpp
@@ -3,10 +3,11 @@
 
 #include "core/manifest.hpp"
 #include "utils/command_helper.hpp"
+#include "utils/constant.hpp"
 namespace yacppm {
 inline void run(bool is_release) {
   Manifest::instance().parse(toml::parse_file("yacppm.toml"));
   build(is_release);
-  run_command("cd build/bin/ && ./" + Manifest::instance().get_info().name);
+  run_command("cd build/" + Constant::get_current_os() + "/bin/ && ./" + Manifest::instance().get_info().name);
 }
 } // namespace yacppm
